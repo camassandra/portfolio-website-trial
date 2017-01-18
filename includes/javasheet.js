9f35801e7f4ecf0a5f59a4e8c27932f5a72215f9
Camassandra <pontecamilla@yahoo.com>
2017-01-18 11:15:37 +0100

    moved first batch of files into new directory

---
+++ includes/javasheet.js
@@ -0,0 +1,19 @@
+
+var clicks = document.getElementsByClassName('more')
+
+var isOpen = function () {
+
+	if (clicks =0, clicks%2===0, onclick=clicks++){
+	console.log("it works")
+}
+}
+/*basically, I want the website to count the times a user clicked on the element. On odd click numbers, a new section will appear under the 'more about my academic background' link, which will not be a link anymore. */
+
+object.onclick = function(){myScript};
+
+document.getElementById("more").onclick = function() {
+	myFunction()};
+
+function myFunction() {
+    document.getElementById("more").innerHTML = "show all the rest of my academic background";
+}
